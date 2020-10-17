7b3c4da4b882d8a26c0309af8273666388664b33
Trevor <rowdeil@gmail.com>
2020-10-17 00:50:29 -0700

    Added the login path and a get_all_users path and corrected some bugs in the token_required function

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,8 @@ from app import app, db
 from flask import request, jsonify, make_response
 from functools import wraps
 from app.models import User, Mailing_Address, Billing_Address
-
+import jwt
+from datetime import datetime, timedelta 
 
 def token_required(f):
     @wraps(f) 
@@ -16,10 +17,13 @@ def token_required(f):
             return jsonify({'message' : 'Token is missing !!'}), 401
    
         try: 
-            # decoding the payload to fetch the stored details 
-            data = jwt.decode(token, app.config['SECRET_KEY']) 
-            current_user = User.query.filter_by(public_id = data['public_id']).first() 
-        except: 
+            # decoding the payload to fetch the stored details
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            print(data)
+            current_user = User.query.filter_by(id = data['id']).first()
+            print(current_user)
+        except Exception as e:
+            print(e)
             return jsonify({ 
                 'message' : 'Token is invalid !!'
             }), 401
@@ -40,43 +44,12 @@ def register():
     """
         Takes a json object structured as so:
         
-        
-        {
-        "User":
-            {
-                "username": "jeeeeffff",
-                "email": "example@email.com",
-                "password": "this_is_secret"
-                "firstName": "Jason",
-                "lastName": "Kyle",
-                "middleName": "",
-                "phoneNumber": "123456789",
-                "gender": "F",
-                "mairtalStatus": "Married"
-            },
-        "MailingAddress":
-            {
-                "streetAddress1": "first street"
-                "streetAddress2": "continuation"
-                "zipCode": "12345"
-                "state": "CA"
-                "country": "United States"
-            }
-        "BillingAddress":
-            {
-                "streetAddress1": "first street"
-                "streetAddress2": "continuation"
-                "zipCode": "12345"
-                "state": "CA"
-                "country": "United States"
-            }
-        }
     """
     # Converts json to python object
     req_data = request.get_json()
 
     # get email, name, password and username
-    user = User.query.filter_by(email = req_data['User']['email']).first()
+    user = User.query.filter_by(username = req_data['User']['username']).first()
 
     if not user:
         # DB ORM object
@@ -117,11 +90,69 @@ def register():
     else:
         return make_response('User already exists. Please Log in.', 202) 
         
-@app.route('/register', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
-    return jsonify("Work in progress")
+    """
+    Logs a user in with username and password
+
+    """
+    # Converts json to python object
+    auth = request.get_json()
+
+    if not auth or not auth.get('username') or not auth.get('password'):
+        # returns 401 if any email or / and password is missing 
+        return make_response( 
+            'Could not verify', 
+            401, 
+            {'WWW-Authenticate' : 'Basic realm ="Login required !!"'} 
+        ) 
+   
+    user = User.query.filter_by(username = auth.get('username')).first() 
+   
+    if not user: 
+        # returns 401 if user does not exist 
+        return make_response( 
+            'Could not verify', 
+            401, 
+            {'WWW-Authenticate' : 'Basic realm ="User does not exist !!"'} 
+        ) 
+   
+    if user.check_password(auth.get('password')): 
+        # generates the JWT Token 
+        token = jwt.encode({ 
+            'id': user.id,
+            'exp' : datetime.utcnow() + timedelta(minutes = 30) 
+        }, app.config['SECRET_KEY']) 
+   
+        return make_response(jsonify({'token' : token.decode('UTF-8')}), 201) 
+    # returns 403 if password is wrong 
+    return make_response( 
+        'Could not verify', 
+        403, 
+        {'WWW-Authenticate' : 'Basic realm ="Wrong Password !!"'} 
+    ) 
 
         
-    
+# User Database Route 
+# this route sends back list of users users 
+@app.route('/user', methods =['GET']) 
+@token_required
+def get_all_users(current_user): 
+    # querying the database 
+    # for all the entries in it 
+    users = User.query.all() 
+    # converting the query objects 
+    # to list of jsons 
+    output = [] 
+    for user in users: 
+        # appending the user data json  
+        # to the response list 
+        output.append({ 
+            'public_id': user.id, 
+            'username' : user.username, 
+            'email' : user.email 
+        }) 
+   
+    return jsonify({'users': output}) 
 
     
