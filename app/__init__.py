7215785d0c00af2dbb7c1ac739190e44f51c60ae
Trevor <rowdeil@gmail.com>
2020-11-13 18:20:39 -0800

    Setting up psycopg2 connection

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,11 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import psycopg2
 
 app = Flask(__name__)
 app.config.from_object(Config)
+conn = psycopg2.connect(os.environ.get('DATABASE_URL'), sslmode="require")
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-
 from app import routes, models
